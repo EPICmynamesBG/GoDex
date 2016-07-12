7a66cd54ea11212cd46696dc3bccc3d762412f9a
JJGayso <gaysojj@rose-hulman.edu>
2016-07-11 21:34:17 -0400

    Added routes for API, ready for mongo db instance to add in pokemon schema

--- back-web/Router/routes.js
+++ back-web/Router/routes.js
@@ -1,9 +1,52 @@
 module.exports = function(app, express) {
   var router = express.Router();
 
+  router.use(function(req, res, next) {
+    console.log("Middleware Firing");
+    next();
+  });
+
   router.get('/', function(req, res) {
     res.json({ message: 'Testing API!'});
   });
+
+  router.route('/AllPokemon')
+    //GETS all pokemon supported
+    .get(function(req, res) {
+      res.json(
+        [
+          { id: 1,
+            name: 'bulbasaur',
+            image: 'http://pokeapi.co/media/sprites/pokemon/1.png'
+          },
+          { id: 2,
+            name: 'test',
+            image: 'http://pokeapi.co/media/sprites/pokemon/2.png'
+          }
+      ]);
+    });
+
+  router.route('/CaughtPokemon')
+    //GETS all pokemon caught
+    .get(function(req, res) {
+      res.json({
+        id: '234234234',
+        pokemon_id: '1',
+        geo_lat: '23423.0103',
+        geo_long: '12809.2098',
+        time: Date.now()
+      });
+    })
+    //POSTS a pokemon to the caught list
+    .post(function(req, res) {
+
+    });
+
+  router.route('/CaughtPokemon/:pokemon_id')
+    //GETS all the pokemon with an id
+    .get(function(req, res) {
+
+    });
 
   var port = process.env.PORT | 8080;
 
